71c8532fcb49dc0c37d8b540e7090ad76dd502d2
Jose Arrisola Jr <37916204+RetroDawg@users.noreply.github.com>
2020-05-12 13:32:59 -0500

    Update crowdstrikeWrapper.sh

--- crowdstrikeWrapper.sh
+++ crowdstrikeWrapper.sh
@@ -2,6 +2,13 @@
 # Developer: Jose Arrisola Jr.
 # Goal: This script will install the crowdstike package
 
+if [ -x /opt/CrowdStrike ]; then
+  echo "CrowdStrike directory exist, please address the service before continuing."
+  exit 1
+else 
+  :
+fi
+
 if [ -x /usr/bin/python3 ]; then
   /usr/bin/python3 crowdstrikeInstaller.py
 else
