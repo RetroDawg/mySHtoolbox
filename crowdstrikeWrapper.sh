006fe06d5135c34bb59c001d13401e7e7abe0d7c
Jose Arrisola Jr <37916204+RetroDawg@users.noreply.github.com>
2020-05-14 13:55:43 -0500

    Update crowdstrikeWrapper.sh

--- crowdstrikeWrapper.sh
+++ crowdstrikeWrapper.sh
@@ -1,23 +1,41 @@
 #!/usr/bin/env bash
 # Developer: Jose Arrisola Jr.
 # Goal: This script is the initial check for crowdstrike and installation
+#Define python paths
+py2_path=`type -P python`
+type -P python
+checkpoint_python2=`echo $?`
+py3_path=`type -P python3`
+type -P python3
+checkpoint_python3=`echo $?`
+
+if ( [ $checkpoint_python2 -ne 0 ] && [ checkpoint_python3 -ne 0 ] ); then
+  echo "No Python version is detected. Exiting."
+  exit 1
+else
+  :
+fi
+
 if [ -x /opt/CrowdStrike ]; then
   echo "CrowdStrike directory exist, please address the service before continuing."
   exit 1
 else 
   :
 fi
+
 curl -o /tmp/crowdstrikeInstaller.py https://repo.geos.tamu.edu/common-configs/toolbox/crowdstrikeInstaller.py && chmod +x /tmp/crowdstrikeInstaller.py
-if [ -x /usr/bin/python3 ]; then
-  /usr/bin/python3 /tmp/crowdstrikeInstaller.py
+
+if [ $checkpoint_python3 -eq 0 ]; then
+  $py3_path /tmp/crowdstrikeInstaller.py
 else
   curl -o /tmp/python3Installer.py https://repo.geos.tamu.edu/common-configs/toolbox/python3Installer.py && chmod +x /tmp/python3Installer.py
-  /usr/bin/python /tmp/python3Installer.py
+  $py2_path /tmp/python3Installer.py
   type -P python3
-  test=`echo $?`
-  if [ $test -eq 0 ]; then
+  checkpoint=`echo $?`
+  if [ $checkpoint -eq 0 ]; then
   echo "python3Installer.py successfully installed python3."
-  /usr/bin/python3 /tmp/crowdstrikeInstaller.py  
+  py3_path=`type -P python3`
+  $py3_path /tmp/crowdstrikeInstaller.py  
   else
     echo "python3Installer.py failed to install python3 exiting."
     exit 1
