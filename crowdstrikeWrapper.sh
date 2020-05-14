da9155e53b2920b3a7621828598f6dbdf32f1eea
Jose Arrisola Jr <37916204+RetroDawg@users.noreply.github.com>
2020-05-14 12:47:58 -0500

    Update crowdstrikeWrapper.sh

--- crowdstrikeWrapper.sh
+++ crowdstrikeWrapper.sh
@@ -13,5 +13,11 @@ if [ -x /usr/bin/python3 ]; then
 else
   curl -o /tmp/python3Installer.py https://repo.geos.tamu.edu/common-configs/toolbox/python3Installer.py && chmod +x /tmp/python3Installer.py
   /usr/bin/python /tmp/python3Installer.py
-  /usr/bin/python3 /tmp/crowdstrikeInstaller.py
+  if [ type -P python3 ]; then
+  echo "python3Installer.py successfully installed python3."
+  /usr/bin/python3 /tmp/crowdstrikeInstaller.py  
+  else
+    echo "python3Installer.py failed to install python3 exiting."
+    exit 1
+  fi
 fi
