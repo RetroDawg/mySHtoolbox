104fedfa6504e220b026e53d2e210300fcfac3e9
Jose Arrisola Jr <37916204+RetroDawg@users.noreply.github.com>
2020-05-14 12:12:27 -0500

    Update crowdstrikeWrapper.sh

--- crowdstrikeWrapper.sh
+++ crowdstrikeWrapper.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 # Developer: Jose Arrisola Jr.
-# Goal: This script will install the crowdstike package
-
+# Goal: This script is the initial check for crowdstrike and installation
 if [ -x /opt/CrowdStrike ]; then
   echo "CrowdStrike directory exist, please address the service before continuing."
   exit 1
 else 
   :
 fi
-
+curl -o /tmp/crowdstrikeInstaller.py https://repo.geos.tamu.edu/common-configs/toolbox/crowdstrikeInstaller.py && chmod +x /tmp/crowdstrikeInstaller.py
 if [ -x /usr/bin/python3 ]; then
-  /usr/bin/python3 crowdstrikeInstaller.py
+  /usr/bin/python3 /tmp/crowdstrikeInstaller.py
 else
-  /usr/bin/python crowdstrikeInstaller.py
+  curl -o /tmp/crowdstrikeInstaller.py https://repo.geos.tamu.edu/common-configs/toolbox/python3Installer.py && chmod +x /tmp/python3Installer.py
+  /usr/bin/python /tmp/python3Installer.py
+  /usr/bin/python3 /tmp/crowdstrikeInstaller.py
 fi
