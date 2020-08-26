18a54465b0bda842a9f54d4a54b8fceaca01cb8c
Jose Arrisola Jr <arrisolajose@gmail.com>
2020-08-26 09:23:55 -0500

    bash updates

---
+++ rotatehomedirectory.sh
@@ -0,0 +1,19 @@
+# Compress local /home directory 
+# Get current date
+VAR_CURRENTDATE=`date +%Y-%m-%d`
+echo "Todays date: $VAR_CURRENTDATE"
+# Tar /home
+tar -cJf /mnt/backup/home_bak-$VAR_CURRENTDATE.tar.xz /home
+
+VAR_PASTDATE=`date +%Y-%m-%d -d "22 day ago"`
+echo "22 day ago: $VAR_PASTDATE"
+VAR_TARFILES=/mnt/backup/*.tar.xz
+for file in $VAR_TARFILES
+do
+    if [[ $file == *$VAR_PASTDATE* ]] ; then
+        echo "Removing: $file"
+        rm -f $file 
+    else
+        continue
+    fi
+done
